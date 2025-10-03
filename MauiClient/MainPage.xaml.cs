2b06855c13426cad1edb37fb841e2b3fae5322ab
Adi Tenea <tenea07adi@yahoo.com>
2025-10-04 02:53:52 +0300

    - Updated learn more button - Created dummy website

--- MauiClient/MainPage.xaml.cs
+++ MauiClient/MainPage.xaml.cs
@@ -6,6 +6,20 @@
         {
             InitializeComponent();
         }
+
+        private async void LearnMore_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Uri uri = new Uri("https://www.autokeep.aditenea.net");
+                await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, e.g., if the browser is not available or the URL is invalid
+                Console.WriteLine($"Error opening browser: {ex.Message}");
+            }
+        }
     }
 
 }
